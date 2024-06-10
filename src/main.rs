4129c0ae50db26b0283c547ab9945d518e459e31
nhubao <baoprokute1234@gmail.com>
2024-06-10 08:55:41 +0700

    Slow requests down

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use futures::future::join_all;
 use itertools::Itertools;
 use std::error::Error;
+use std::sync::atomic::{AtomicUsize, Ordering};
 
 mod cloudflare;
 mod utils;
@@ -62,42 +62,82 @@ async fn exec() -> Result<(), Box<dyn Error>> {
     println!("Deleted {deleted_policy} gateway policies");
 
     // Delete all lists parallely tokio
-    let delete_list_tasks = cf_lists.as_ref().and_then(|lists| {
-        Some(
-            lists
-                .iter()
-                .filter_map(|list| {
-                    let name = list["name"].as_str();
-                    let id = list["id"].as_str();
-                    match (name, id) {
-                        (Some(name), Some(id)) => {
-                            println!("Deleting list {name} - ID:{id}");
-                            Some(cloudflare::delete_cf_list(id.as_ref()))
-                        }
-                        _ => None,
-                    }
-                })
-                .collect::<Vec<_>>(),
-        )
-    });
+    // let delete_list_tasks = cf_lists.as_ref().and_then(|lists| {
+    //     Some(
+    //         lists
+    //             .iter()
+    //             .filter_map(|list| {
+    //                 let name = list["name"].as_str();
+    //                 let id = list["id"].as_str();
+    //                 match (name, id) {
+    //                     (Some(name), Some(id)) => {
+    //                         println!("Deleting list {name} - ID:{id}");
+    //                         Some(cloudflare::delete_cf_list(id.as_ref()))
+    //                     }
+    //                     _ => None,
+    //                 }
+    //             })
+    //             .collect::<Vec<_>>(),
+    //     )
+    // });
 
-    if let Some(tasks) = delete_list_tasks {
-        join_all(tasks).await;
+    // if let Some(tasks) = delete_list_tasks {
+    //     join_all(tasks).await;
+    // }
+
+    let request_count: AtomicUsize = AtomicUsize::new(0);
+
+    if let Some(lists) = cf_lists.as_ref() {
+        for list in lists.iter() {
+            let name = list["name"].as_str();
+            let id = list["id"].as_str();
+            match (name, id) {
+                (Some(name), Some(id)) => {
+                    let current_count = request_count.fetch_add(1, Ordering::Relaxed);
+                    if current_count > 4 {
+                        let now = tokio::time::Instant::now();
+                        let elapsed = 1000 - now.elapsed().as_millis();
+                        tokio::time::sleep(tokio::time::Duration::from_millis(elapsed as u64))
+                            .await;
+                        request_count.store(0, Ordering::Relaxed);
+                    }
+                    println!("Deleting list {name} - ID:{id}");
+                    cloudflare::delete_cf_list(id.as_ref()).await;
+                }
+                _ => {}
+            }
+        }
     }
 
-    tokio::time::sleep(tokio::time::Duration::from_secs(60 * 5)).await;
+    // tokio::time::sleep(tokio::time::Duration::from_secs(60 * 5)).await;
 
     // Create cf list by chunk of 1000 with name containing incremental number
-    let create_list_tasks = black_list
-        .chunks(1000)
-        .enumerate()
-        .map(|(i, chunk)| {
-            let name = format!("{cf_prefix} {i}");
-            let chunk_str_refs = chunk.iter().map(|&s| s).collect::<Vec<_>>();
-            return cloudflare::create_cf_list(name, chunk_str_refs);
-        })
-        .collect::<Vec<_>>();
-    let new_cf_list = join_all(create_list_tasks).await;
+    // let create_list_tasks = black_list
+    //     .chunks(1000)
+    //     .enumerate()
+    //     .map(|(i, chunk)| {
+    //         let name = format!("{cf_prefix} {i}");
+    //         let chunk_str_refs = chunk.iter().map(|&s| s).collect::<Vec<_>>();
+    //         return cloudflare::create_cf_list(name, chunk_str_refs);
+    //     })
+    //     .collect::<Vec<_>>();
+    // let new_cf_list = join_all(create_list_tasks).await;
+
+    let request_count: AtomicUsize = AtomicUsize::new(0);
+    let mut new_cf_list: Vec<Option<serde_json::Value>> = Vec::new();
+    for (i, chunk) in black_list.chunks(1000).enumerate() {
+        let name = format!("{cf_prefix} {i}");
+        let chunk_str_refs = chunk.iter().map(|&s| s).collect::<Vec<_>>();
+        let current_count = request_count.fetch_add(1, Ordering::Relaxed);
+        if current_count > 4 {
+            let now = tokio::time::Instant::now();
+            let elapsed = 1000 - now.elapsed().as_millis();
+            tokio::time::sleep(tokio::time::Duration::from_millis(elapsed as u64)).await;
+            request_count.store(0, Ordering::Relaxed);
+        }
+        new_cf_list.push(cloudflare::create_cf_list(name, chunk_str_refs).await);
+    }
+
     let new_cf_list_ids = new_cf_list
         .iter()
         .filter_map(|l| Some(l.as_ref()?.get("id")?.as_str()?.to_owned()))
