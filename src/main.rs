009eaf06c3e7cb654fcf7e2eedb36d648fcd91a5
nhubao <baoprokute1234@gmail.com>
2024-06-11 01:20:11 +0700

    Update lists and wait time

--- src/main.rs
+++ src/main.rs
@@ -95,7 +95,7 @@ async fn exec() -> Result<(), Box<dyn Error>> {
                 }
                 _ => {}
             }
-            tokio::time::sleep(tokio::time::Duration::from_secs(10)).await;
+            tokio::time::sleep(tokio::time::Duration::from_secs(5)).await;
         }
     }
 
@@ -118,7 +118,7 @@ async fn exec() -> Result<(), Box<dyn Error>> {
         let name = format!("{cf_prefix} {i}");
         let chunk_str_refs = chunk.iter().map(|&s| s).collect::<Vec<_>>();
         new_cf_list.push(cloudflare::create_cf_list(name, chunk_str_refs).await);
-        tokio::time::sleep(tokio::time::Duration::from_secs(10)).await;
+        tokio::time::sleep(tokio::time::Duration::from_secs(5)).await;
     }
 
     let new_cf_list_ids = new_cf_list
